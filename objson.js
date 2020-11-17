c4b366312c9c1d4d80ee8843a215b0f414f2820f
EjangNurjaman <34273125+EjangNurjaman@users.noreply.github.com>
2020-11-17 11:56:57 +0700

    Create objson.js

---
+++ objson.js
@@ -0,0 +1,82 @@
+<script>
+var objJson = [{
+		"height": "486",
+		"width": "345",
+		"id": "576812664764632428",
+		"tran": "&quot;Orang bilang kita tidak bisa hidup tanpa CINTA&quot;<br />&quot;Ku pikir OKSIGEN lebih penting !!&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "445",
+		"width": "345",
+		"id": "579838520770410242",
+		"tran": "&quot;memiliki hati yang lembut di dunia yang kejam, Bukankah itu keberanian kelemahan &quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "714",
+		"width": "345",
+		"id": "67342956917189493",
+		"tran": "&quot;BAHKAN DALAM HIDUPKU SENDIRI. AKU TIDAK INGIN JADI KARAKTER UTAMA&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "445",
+		"width": "345",
+		"id": "275775177171762892",
+		"tran": "&quot;Kami tidak akan pernah menang jika kami tidak percaya kami bisa.&quot;",
+		"cite": "Daichi Sawamura"
+	},
+
+	{
+		"height": "611",
+		"width": "345",
+		"id": "250653535500254540",
+		"tran": "&quot;Orang kuat bukanlah orang yang tidak menangis, orang yang kuat adalah orang yang menangis dan meneteskan air mata sesaat, lalu bangkit dan bertarung lagi&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "721",
+		"width": "345",
+		"id": "633318766332891332",
+		"tran": "&quot;Ini adalah kisahmu&quot;<br />&quot;Ini adalah ketakutanmu&quot;<br />&quot;Jangan pernah lupakan&quot;<br />&quot;Siapa kau&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "509",
+		"width": "345",
+		"id": "362539838758337713",
+		"tran": "&quot;AKU SELESAI&quot; <br />&quot;Aku sudah cukup menyia-nyiakan AIR MATA&quot; <br />&quot;UNTUK ORANG YANG BAHKAN TIDAK PEDULI&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "359",
+		"width": "345",
+		"id": "630363279089365910",
+		"tran": "&quot;Aku punya impian untuk diraih, dan jika aku harus mati karena berusaha ... setidaknya aku sudah mencoba&quot;",
+		"cite": "Monkey D. Luffy"
+	},
+
+	{
+		"height": "414",
+		"width": "345",
+		"id": "679058450053601122",
+		"tran": "&quot;Jika salah mencintaimu, maka hatiku tidak akan membiarkan aku benar&quot;",
+		"cite": ""
+	},
+
+	{
+		"height": "433",
+		"width": "345",
+		"id": "132926626490925381",
+		"tran": "&quot;Mengapa aku harus meminta maaf karena telah menjadi monster&quot;<br />&quot;ketika kau tidak meminta maaf karena telah menyakitiku?&quot;",
+		"cite": ""
+	}
+];
+</script>
